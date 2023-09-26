46b5f5312e4acb4dcb0b1be40b5532052eb27e5a
Anas Gamal <agamal@stetson.edu>
2023-09-26 03:37:13 -0400

    make it not depend on strapi being running

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { Routes, Route } from "react-router-dom"
 import useFetch from "./hooks/useFetch"
 
 export default function App() {
-  let {loading, error, data} = useFetch("http://localhost:1337/api/blogs?populate=*")
+  // let {loading, error, data} = useFetch("http://localhost:1337/api/blogs?populate=*")
+  let data = undefined;
+  // if (loading)  return <div>Loading...</div>
+  // if (error) return <div>Something went wrong: {error}</div>
 
-  if (loading)  return <div>Loading...</div>
-  if (error) return <div>Something went wrong: {error}</div>
-
-  console.log(data)
+  // console.log(data)
   return (
     <div>
       <Navbar />
